de8613f9c9a3b4411d701bbabf612ead807bd156
J0ss077 <jossmilo133@gmail.com>
2026-05-24 17:18:13 -0500

    almost done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Audio Importer
+
+A simple mod for importing audios/music/soundFXs into Factorio, mainly made for non-modders and being used in servers (to play music and prank your friends xd)
--- data-updates.lua
+++ data-updates.lua
@@ -1,13 +1,20 @@
 local notes = {}
 
 local function import(root)
-
+    ---
     for __, file in ipairs(require(root..".".."F077-sai-index")) do
         --
         table.insert(notes, { name = file, sound = { filename = root.."/audios/"..file } })
+        --
     end
 end
 
-for name, __ in pairs(mods) do if pcall(import, "__"..name.."__") then log("imported '"..name.."'") end end
+for name, __ in pairs(mods) do
+    --
+    if pcall(import, "__"..name.."__") then log("imported '"..name.."'") end
+    --
+end
 
-table.insert(data.raw["programmable-speaker"]["programmable-speaker"].instruments, { name = "simple-audio-importer", notes = notes })
+local speaker = data.raw["programmable-speaker"]["programmable-speaker"]
+
+table.insert(speaker.instruments, { name = "simple-audio-importer", notes = notes })
